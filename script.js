93ee7967103ce9ef6298518226d3c16d86d029ca
Dexzinn <pedhenriquef@gmail.com>
2023-02-26 19:39:52 -0300

    css - menu - android
    alterei css, coloquei menu android e tals

---
+++ script.js
@@ -0,0 +1,14 @@
+var potinhos = document.getElementById('icon')
+
+function menuClicar() {
+    if(menu_medicos.style.display == 'block') {
+        menu_medicos.style.display = 'none'
+        potinhos.style.position = 'static';
+    } else {
+        menu_medicos.style.display = 'block'
+        potinhos.style.position = 'absolute';
+        potinhos.style.top = '80px';
+        potinhos.style.left = '200px';
+    }
+
+}
